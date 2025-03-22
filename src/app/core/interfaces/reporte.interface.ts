132bd28e30a922d9838f477b7b59e98a6aa7b14f
juandavidcertuche93 <juandavidcertuchequijano2@gmail.com>
2025-03-22 12:14:08 -0500

    Implemented reporting feature with report interface and service for sales data.

---
+++ src/app/core/interfaces/reporte.interface.ts
@@ -0,0 +1,11 @@
+export interface Reporte {
+  numeroVenta:string,
+  tipoPago:string,
+  fechaRegistro:string,
+  totalVenta:string,
+  producto:string,
+  cantidad:number,
+  precio:string,
+  total:string,
+  usuario:string
+}
